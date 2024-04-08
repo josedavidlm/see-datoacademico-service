818026a09cc86c3b93dcff76110e0eb0d193da27
Jose David Lagos Morales <consultor_dgpmacdf32@mef.gob.pe>
2024-04-07 23:34:35 -0500

    configuracion test

--- src/main/java/pe/com/cayetano/see/dataacademico/SeeDataAcademicoApplication.java
+++ src/main/java/pe/com/cayetano/see/dataacademico/SeeDataAcademicoApplication.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @OpenAPIDefinition
+/*@EnableDiscoveryClient*/
 public class SeeDataAcademicoApplication {
 
 	public static void main(String[] args) {
